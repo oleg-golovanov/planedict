74c6247ccb9a725a37fccb88f7e847a7281d213c
Oleg Golovanov <golovanov.ov@gmail.com>
2015-09-18 14:00:11 +0500

    updated classifiers

--- setup.py
+++ setup.py
@@ -21,9 +21,17 @@ setup(
     long_description=open(join(dirname(__file__), 'README.rst')).read(),
     test_suite='tests.tests',
     classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ]
 )
